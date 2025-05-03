978dd06a25c646e1441b8f792f9ab945b6d430e9
thomasmacharia3 <thomasmacharia3@gmail.com>
2025-05-03 18:55:53 +0300

    Delete templates directory

--- templates/auth/logout.php
+++
@@ -1,9 +0,0 @@
-<?php
-require_once '../user/Session.php';
-
-Session::start();
-Session::destroy(); // Destroy the session
-
-header("Location: login.php"); // Redirect to login page
-exit();
-?>
--- templates/results.php
+++
@@ -1,4 +0,0 @@
-<?php require_once('./inc/header.php') ?>
-
-
-<?php require_once('./inc/footer.php') ?>
